70dc85ade3ef0d522f6e3082be7a74eba26048a9
aadithakur7778-star <aadithakur7778@gmail.com>
2025-11-06 14:14:57 +0530

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add JavaScript for dynamic elements, like animations or messages
+// For a simple static page, you can leave this file empty or add a simple console log
+console.log("Diwali greetings loaded!");
+
+// Example: Add a simple firework effect (requires more complex code or a library)
+// For a basic page, this is not necessary.
